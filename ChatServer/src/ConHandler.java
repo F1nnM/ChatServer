ef64287586f856934880261ca7f213aa8f7a57b5
Finn <Finn@Finn-Lappo>
2017-05-26 16:48:06 +0200

    dfgdfg

---
+++ ChatServer/src/ConHandler.java
@@ -0,0 +1,54 @@
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class ConHandler implements Runnable {
+
+	private Socket socket;
+	private boolean run = false;
+	private Scanner in;
+	private PrintWriter out;
+	private ArrayList<String> ToWrite;
+
+	public ConHandler(Socket socket) {
+		this.socket = socket;
+	}
+
+	public String getIP() {
+		return socket.getRemoteSocketAddress().toString();
+	}
+
+	public void stop() {
+		run = false;
+	}
+
+	public void send(String str) {
+		ToWrite.add(str);
+	}
+
+	@Override
+	public void run() {
+		ToWrite = new ArrayList<String>();
+		try {
+			run = true;
+			in = new Scanner(socket.getInputStream());
+			out = new PrintWriter(socket.getOutputStream(), true);
+			String input;
+			while (run) {
+				if (ToWrite.size() > 0)
+					out.write(ToWrite.get(0));
+				if ((input = in.nextLine()) != null) {
+					String[] parts = input.split("-");
+					if (!(Main.sendTo(parts[0], parts[1]))) {
+						send("Invalid IP");
+					}
+
+				}
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+}
---
+++ ChatServer/src/Main.java
@@ -0,0 +1,31 @@
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.util.ArrayList;
+
+public class Main {
+
+	private static ArrayList<ConHandler> cons;
+
+	public static void main(String[] args) throws IOException {
+		ServerSocket ss = new ServerSocket(50000);
+		cons = new ArrayList<ConHandler>();
+		while (true) {
+			try {
+				ConHandler con = new ConHandler(ss.accept());
+				cons.add(con);
+				new Thread(con).start();
+				System.out.println("Connected to: "+con.getIP());
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+	}
+	
+	public static boolean sendTo(String msg, String IP) {
+		for (ConHandler con : cons) {
+			if(con.getIP()==IP)con.send(msg);
+			return true;
+		}
+		return false;
+	}
+}
