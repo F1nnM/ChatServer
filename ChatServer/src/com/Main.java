00b8ab3eafa7e236aa0c493bdd82966fbe098eba
Finn <Finn@Finn-Lappo>
2017-06-18 16:54:46 +0200

    removed some more warnings

--- ChatServer/src/com/Main.java
+++ ChatServer/src/com/Main.java
@@ -1,12 +1,9 @@
 package com;
 
 import java.io.IOException;
-import java.net.InetAddress;
 import java.net.ServerSocket;
 import java.sql.SQLException;
 import java.util.ArrayList;
-
-import com.mysql.jdbc.StringUtils;
 
 import utils.SqlTools;
 
