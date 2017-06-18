2daa3bd4d2a34b5ba436a55b75c3cf534c0db0bb
Finn <Finn@Finn-Lappo>
2017-06-18 16:54:02 +0200

    removed warnings and UdpServer

--- ChatServer/src/com/Main.java
+++ ChatServer/src/com/Main.java
@@ -21,10 +21,6 @@ public class Main implements Runnable {
 			ss = new ServerSocket(23456);
 			cons = new ArrayList<ConHandler>();
 			while (true) {
-				for (ConHandler con : cons) {
-					System.out.println(cons.size());
-					System.out.flush();
-				}
 				try {
 					ConHandler con = new ConHandler(ss.accept());
 					cons.add(con);
@@ -40,14 +36,12 @@ public class Main implements Runnable {
 	}
 
 	public static void newMessage(int ID) throws InstantiationException, IllegalAccessException, ClassNotFoundException, SQLException {
-		int i = 0;
 		for (ConHandler con : cons) {
 			if (con.getAddress().toString().equals(SqlTools.getIp(ID))){
 				con.newMessage();
 				System.out.println("notified"+con.getAddress().toString());
 				break;
 			}
-			i++;
 		}		
 	}
 	
